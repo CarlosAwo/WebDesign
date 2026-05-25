a1b3e7cd081ceb457ca340ee2a68d2b0e79c8143
Carlos Awo <awocarlos55@gmail.com>
2026-05-25 14:18:53 +0100

    fix(view_component): move blob helper access to before_render

--- app/components/v1/form/elements/file_field_component.rb
+++ app/components/v1/form/elements/file_field_component.rb
@@ -3,38 +3,28 @@ class V1::Form::Elements::FileFieldComponent < V1::Form::Elements::BaseComponent
     super
 
     @hint        = @metadata[:hint]
-    @accept      = @metadata[:accept] || ""
+    @accept      = @metadata[:accept] || "image/*"
     @upload_text = @metadata[:upload_text] || "Choisir"
+    @file_url    = @metadata[:file_url]
+    @file_name   = @metadata[:file_name]
+  end
 
-    # Priorité aux valeurs explicites
-    @file_url  = @metadata[:file_url].presence
-    @file_name = @metadata[:file_name].presence
-
-    return if @file_url.present? && @file_name.present?
-
-    extract_file_from_object
+  def before_render
+    extract_file_from_object unless @file_url
   end
 
   private
 
   def extract_file_from_object
-    object = form.object
+    return unless @form&.object&.respond_to?(@field_name)
 
-    return unless object.present?
-    return unless @field_name.present?
-    return unless object.respond_to?(@field_name)
-
-    file = object.public_send(@field_name)
+    file = @form.object.public_send(@field_name)
 
     return unless file.respond_to?(:attached?)
     return unless file.attached?
-    return unless file.blob.persisted? # ← guard ajouté
+    return unless file.blob.persisted?
 
-    @file_url ||= Rails.application.routes.url_helpers.rails_blob_path(
-      file,
-      only_path: true
-    )
-
-    @file_name ||= file.filename.to_s
+    @file_url  = helpers.rails_blob_path(file, only_path: true)
+    @file_name = file.blob.filename.to_s
   end
 end
